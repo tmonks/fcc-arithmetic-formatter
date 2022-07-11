564d5068882bce9d83b02856aac3d2fd98caa735
Tom Monks <monksy@gmail.com>
2022-07-11 11:22:30 -0400

    Pad and arrange problems

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,33 +1,72 @@
 import re
 
 
-def arithmetic_arranger(problems):
-    arranged_problems = []
+def arithmetic_arranger(problems, solve=False):
+    """ Arranges a list of arithmetic problems vertically """
+
     if len(problems) > 4:
         return "Error: Too many problems."
 
+	# split and solve problems
     try:
-        parsed = parse_problems(problems)
+        problem_tuples = split_and_solve(problems)
     except Exception as e:
         return str(e)
 
-    return arranged_problems
+    # pad each problem to the correct string length
+    padded = list(map(lambda p: pad_problem(p, solve), problem_tuples))
+
+    return arrange_problems(padded, solve)
 
 
-def parse_problems(problems):
-    parsed = []
+def split_and_solve(problems):
+    return list(map(parse, problems))
 
-    for problem in problems:
-        [x, op, y] = problem.split()
-        if not is_operator_valid(op):
-            raise Exception("Error: Operator must be '+' or '-'.")
-        if not is_digits_only(x) or not is_digits_only(y):
-            raise Exception("Error: Numbers must only contain digits.")
-        if not is_length_ok(x) or not is_length_ok(y):
-            raise Exception("Error: Numbers cannot be more than four digits.")
+def parse(problem):
+    [x, op, y] = problem.split()
 
-    return parsed
+    if not is_operator_valid(op):
+        raise Exception("Error: Operator must be '+' or '-'.")
+    if not is_digits_only(x) or not is_digits_only(y):
+        raise Exception("Error: Numbers must only contain digits.")
+    if not is_length_ok(x) or not is_length_ok(y):
+        raise Exception("Error: Numbers cannot be more than four digits.")
 
+    return (x, op, y, solve_problem(x,op,y))
+
+
+def solve_problem(x, op, y):
+    if op == "+":
+        return str(int(x) + int(y))
+    else:
+        return str(int(x) - int(y))
+
+def pad_problem(problem, solve=False):
+    (x, op, y, z) = problem
+    if solve:
+        max_length = max(len(x), len(y), len(z))
+    else:
+        max_length = max(len(x), len(y))
+
+    top = x.rjust(max_length + 2, ' ')
+    middle = op + ' ' + y.rjust(max_length, ' ')
+    line = '-' * (max_length + 2)
+    bottom = z.rjust(max_length + 2, ' ')
+
+    if solve:
+        return (top, middle, line, bottom)
+    else:
+        return (top, middle, line)
+
+def arrange_problems(padded_problems, solve=False):
+    arranged = "    ".join(map(lambda x: x[0], padded_problems)) + "\n"
+    arranged += "    ".join(map(lambda x: x[1], padded_problems)) + "\n"
+    arranged += "    ".join(map(lambda x: x[2], padded_problems)) + "\n"
+
+    if solve:
+        arranged += "    ".join(map(lambda x: x[3], padded_problems)) + "\n"
+
+    return arranged 
 
 def is_operator_valid(op):
     return op in ['-', '+']
@@ -42,3 +81,11 @@ def is_digits_only(number):
 
 def is_length_ok(number):
     return len(number) <= 4
+
+
+def format_problem(problem):
+    max_length = get_max_length(problem)
+    
+
+def get_max_length(problem_tuple):
+    return max(map(len, problem_tuple))
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from pytest import main
 
-from arithmetic_arranger import arithmetic_arranger
+# from arithmetic_arranger import arithmetic_arranger
+# from arithmetic_arranger import solve
+import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger.arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]), True)
+# print(arithmetic_arranger.parse('32 + 698'))
+# print(arithmetic_arranger.pad_problem(('32', '+', '698', '1024'), True))
 
 
 # Run unit tests automatically
-main(['-vv'])
+# main(['-vv'])
